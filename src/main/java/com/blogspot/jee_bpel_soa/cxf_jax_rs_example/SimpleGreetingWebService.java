96fd07646f9055b684bd04db60915143bf383182
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-05-11 06:37:39 +0200

    jee-bpel-soa.blogspot.com CXF JAX-RS simple example

---
+++ src/main/java/com/blogspot/jee_bpel_soa/cxf_jax_rs_example/SimpleGreetingWebService.java
@@ -0,0 +1,37 @@
+package com.blogspot.jee_bpel_soa.cxf_jax_rs_example;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.PathSegment;
+
+@Path("/greetings")
+@Produces("text/plain")
+public class SimpleGreetingWebService {
+	
+	@GET
+	@Path("/name/{name}")
+	public String sayHi(@PathParam("name") String name) {
+		return "Hi " + name + "! How are you!";
+	}
+
+	@GET
+	@Path("/name/{name}/state/{state}")
+	public String sayHi(@PathParam("name") String name,
+			@PathParam("state") String state) {
+		return sayHi(name) + " Do you like " + state + "?";
+	}
+
+	@GET
+	@Path("/name/{name}/state/{state}/{params:.*}")
+	public String sayHi(@PathParam("name") String name,
+			@PathParam("state") String state,
+			@PathParam("params") String paramsAsString,
+			@PathParam("params") List<PathSegment> paramsAsList) {
+		return sayHi(name, state) + " You like to pass params? "
+				+ paramsAsString;
+	}
+}
